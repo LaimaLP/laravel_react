4b9c878d355520448d4ae99d1b37d65d4e464275
LaimaLP <pileliene.laima@gmail.com>
2024-02-14 15:33:37 +0200

    laravel react init

---
+++ react-laravel/resources/js/Pages/Animal.jsx
@@ -0,0 +1,8 @@
+export default function Animal({color}){
+    return (
+        <h1 style={{
+            color:color
+        }}
+        >Animal</h1>
+    );
+}
---
+++ react-laravel/resources/views/animal.blade.php
@@ -0,0 +1,4 @@
+@extends('app2')
+@section('content')
+    <h1 style="color: {{$color}}">Animal</h1>
+@endsection('content')
